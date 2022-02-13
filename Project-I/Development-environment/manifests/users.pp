ae53c6e20797d7a19248ee78847ece1f50aa9360
robellm <72279738+robellm@users.noreply.github.com>
2022-02-13 15:45:49 +0100

    Update users.pp

--- Project-I/Development-environment/manifests/users.pp
+++ Project-I/Development-environment/manifests/users.pp
@@ -3,22 +3,23 @@ group { 'developers':
 
 user { 'bob':
       ensure => present,
+      home => '/home/bob',
       groups => ['sudo'],
       comment => "This is Bobby's account"
-      home => '/home/bob',
       managehome => true,
       password => 'BobbyB', }
 
 user { 'janet':
       ensure => present,
+      home => '/home/janet',
       groups => ['sudo', 'developers'],
       comment => "This is Janet's account"
       managehome => true,
-      home => '/home/janet',
       password => 'JennyJ', }
 
 user { 'alice':
       ensure => present,
+      home => '/home/alice',
       groups => ['sudo'],
       comment => "This is Alice from Wonderland"
       managehome => true,
@@ -26,6 +27,7 @@ user { 'alice':
 
 user { 'tim':
       ensure => present,
+      home => '/home/tim',
       groups => ['sudo', 'developers'],
       comment => "This is Tims' account"
       managehome => true,
