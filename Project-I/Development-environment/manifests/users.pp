32049debbc48f61ff2e3b5b63704626207ba0da6
robellm <72279738+robellm@users.noreply.github.com>
2022-02-19 10:55:09 +0100

    Update users.pp

--- Project-I/Development-environment/manifests/users.pp
+++ Project-I/Development-environment/manifests/users.pp
@@ -1,35 +1,36 @@
-group { 'developers':
-        ensure => present, }
+class users {
+        group { "developers":
+                ensure => present, }
 
-user { 'bob':
-      ensure => present,
-      home => '/home/bob',
-      groups => ['sudo'],
-      comment => "This is Bobby's account"
-      managehome => true,
-      password => 'BobbyB', }
+        user { "bob":
+                ensure => present,
+                home => '/home/bob',
+                shell => '/bin/bash',
+                groups => ["sudo"],
+                comment => "This is Bobbys' account",
+                password => '$1$U0KD8Cmb$xNfIcFDHDit8GcfjPH6UZ.', }
 
-user { 'janet':
-      ensure => present,
-      home => '/home/janet',
-      groups => ['sudo', 'developers'],
-      comment => "This is Janet's account"
-      managehome => true,
-      password => 'JennyJ', }
+        user { "janet":
+                ensure => present,
+                home => '/home/janet',
+                shell => '/bin/bash',
+                groups => ["sudo", "developers"],
+                comment => "This is Janets' account",
+                password => '$1$L7npvSSD$pCL5NI6Trt0G9zlnJMy7Y.', }
 
-user { 'alice':
-      ensure => present,
-      home => '/home/alice',
-      groups => ['sudo'],
-      comment => "This is Alice from Wonderland"
-      managehome => true,
-      password => 'Alice', }
+        user { "alice":
+                ensure => present,
+                home => '/home/alice',
+                shell => '/bin/bash',
+                groups => ["sudo"],
+                comment => "This is Alice account",
+                password => '$1$OcFCcwWm$IHLoomw9mGrPQ//3uMPjR1', }
 
-user { 'tim':
-      ensure => present,
-      home => '/home/tim',
-      groups => ['sudo', 'developers'],
-      comment => "This is Tims' account"
-      managehome => true,
-      password => 'TimmyT', }
-      
+        user { "tim":
+                ensure => present,
+                home => '/home/tim',
+                shell => '/bin/bash',
+                groups => ["sudo", "developers"],
+                comment => "This is Tims' account",
+                password => '$1$J2kc1BoM$ryDl0hNdEsn8CasYhdB3a.', }
+}
