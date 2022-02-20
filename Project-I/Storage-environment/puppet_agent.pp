51a1c54b2d71c3d7a628fdb7b254e7986cb315c9
robellm <72279738+robellm@users.noreply.github.com>
2022-02-20 17:37:47 +0100

    Create puppet_agent.pp

---
+++ Project-I/Storage-environment/puppet_agent.pp
@@ -0,0 +1,29 @@
+$master = "development-environment.openstacklocal"
+$master_ip = "10.0.65.105"
+$interval = "600"
+
+host { 'development-environment.openstacklocal':
+        name => $master,
+        ensure => present,
+        host_aliases => "development-environment",
+        ip => $master_ip
+}
+augeas { "puppet_default":
+        context => "/files/etc/default/puppet",
+        changes => ["set START yes"],
+        notify => Service['puppet'],
+}
+augeas { "puppet.conf":
+        context => "/files/etc/puppetlabs/puppet/puppet.conf",
+        changes => [
+                "set agent/server $master",
+                "set agent/runinterval 600",
+                ],
+        notify => Service['puppet'],
+}
+service { "puppet":
+        ensure => running,
+        hasrestart => true,
+        hasstatus => true,
+        require => Augeas['puppet.conf','puppet_default'],
+}
