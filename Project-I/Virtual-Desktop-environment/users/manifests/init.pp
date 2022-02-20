6e17b912ae440a5ca7234e93597eb667d8ffca4b
robellm <72279738+robellm@users.noreply.github.com>
2022-02-20 20:36:08 +0100

    Create init.pp

---
+++ Project-I/Virtual-Desktop-environment/users/manifests/init.pp
@@ -0,0 +1,33 @@
+class users {
+
+         user { "john":
+                ensure => present,
+                home => '/home/john',
+                shell => '/bin/bash',
+                managehome => true,
+                comment => " This is Johns' account ",
+                password => '', }
+                
+         user { "alice":
+                ensure => present,
+                home => '/home/alice',
+                shell => '/bin/bash',
+                managehome => true,
+                comment => " This is Alices' account ",
+                password => '', }
+                  
+         user { "bob":
+                ensure => present,
+                home => '/home/bob',
+                shell => '/bin/bash',
+                managehome => true,
+                comment => " This is Bobs' account ",
+                password => '', }
+                
+         user { "kate":
+                ensure => present,
+                home => '/home/kate',
+                shell => '/bin/bash',
+                managehome => true,
+                comment => " This is Kates' account ",
+                password => '', }
