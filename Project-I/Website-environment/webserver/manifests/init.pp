d368973dac704270e90d7d69514606497aca014b
robellm <72279738+robellm@users.noreply.github.com>
2022-02-20 20:54:54 +0100

    Create init.pp

---
+++ Project-I/Website-environment/webserver/manifests/init.pp
@@ -0,0 +1,11 @@
+class webserver {
+
+          package { "apache2":
+                    ensure => present, }
+          
+          package { "php5":
+                    ensure => present, }
+                    
+          package { "php-mysql":
+                    ensure => present, }
+ }
