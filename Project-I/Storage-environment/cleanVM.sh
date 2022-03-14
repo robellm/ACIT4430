30108e317bdf1707c5e316d7e2290c4bae768406
robellm <72279738+robellm@users.noreply.github.com>
2022-03-14 16:52:29 +0100

    Create cleanVM.sh

---
+++ Project-I/Storage-environment/cleanVM.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo -e "\n"
+echo " ------------ ....... STOPPING All Instances ....... ------------ "
+echo -e "\n"
+sleep 3
+mln stop -p project
+echo -e "\n"
+sleep 2
+echo " ------------ ....... ALL Instances Stopped ....... ------------ "
+sleep 4
+
+echo -e "\n"
+echo " ------------ ....... REMOVING CERTIFICATES FROM AGENTS ....... ----------- "
+echo -e "\n"
+sleep 3
+/opt/puppetlabs/bin/puppetserver ca clean --certname vm-webserver
+/opt/puppetlabs/bin/puppetserver ca clean --certname vm-backup-server
+/opt/puppetlabs/bin/puppetserver ca clean --certname vm-storage-server-1
+/opt/puppetlabs/bin/puppetserver ca clean --certname vm-storage-server-2
+echo -e "\n"
+sleep 3
+echo " -------------- ......... Certificates REMOVED! ......... ---------------- "
+echo -e "\n"
+sleep 4
+
+echo " -------- ********* REMOVING INSTANCES, PLEASE PRESS y then ENTER WHEN IT REQUESTS YOU! ********* ---------- "
+sleep 3
+mln remove -p project
